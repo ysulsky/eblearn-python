027fbdbc5a51401ab2f04d4cafdcf24dea4132cb
Yury Sulsky <yury.sulsky@gmail.com>
2010-07-22 18:53:13 -0400

    added unfold, narrow, and select added convolve (from scipy.signal)

--- eblearn/util.py
+++ eblearn/util.py
@@ -3,6 +3,7 @@ import scipy as sp
 import os, pickle, re, tempfile
 from math import pi, sqrt
 from _util import rtype
+from scipy.signal import correlate as sp_correlate
 
 sp.seterr('raise')
 
@@ -13,6 +14,42 @@ ones  = lambda shape: sp.ones(shape, rtype, 'C')
 product = sp.prod
 sqmag  = lambda x: sp.square(x).sum()
 sqdist = lambda a, b: sqmag(a - b)
+
+def convolve(x, k, out=None):
+    y = sp_correlate(x, k, 'valid')
+    if out is not None: out[:] = y
+    else:               out    = y
+    return out
+
+try:
+    from numpy.lib.stride_tricks import as_strided
+except ImportError:
+    # this version won't work on zero-size arrays
+    def as_strided(x, shape, strides):
+        return sp.ndarray.__new__(sp.ndarray, strides=strides, shape=shape,
+                                  buffer=x, dtype=x.dtype)
+
+def unfold(x, dim, size, step):
+    assert (step > 0)
+    dimlen = x.shape[dim]
+    n = (dimlen - size) // step
+    if dimlen < size or n * step != dimlen - size:
+        if dimlen > size:
+            dimlen = size + n * step
+        else:
+            n, dimlen = -1, 0
+        x = x.swapaxes(-1, dim)[...,:dimlen].swapaxes(-1, dim)
+    s = x.strides[dim]
+    newshape   = x.shape[:dim]   + (n+1,)    + x.shape[dim+1:]   + (size,)
+    newstrides = x.strides[:dim] + (step*s,) + x.strides[dim+1:] + (s,)
+    return as_strided(x, shape=newshape, strides=newstrides)
+
+def select(x, dim, idx):
+    if dim == 0: return x[idx]
+    return x.swapaxes(0,dim)[idx].swapaxes(0,dim-1)
+
+def narrow(x, dim, size, offset = 0):
+    return x.swapaxes(0,dim)[offset:offset+size].swapaxes(0,dim)
 
 def dtanh(x):
     e = x.clip(-8, 8)
